ceabbd651f3c8141d89c02a53bd7a6713b67545c
wesleyangeli <wesleyangeli@gmail.com>
2022-08-20 15:59:46 -0300

    aula 1

---
+++ menorValor.js
@@ -0,0 +1,13 @@
+const livros = require("./listaLivros");
+
+let maisBarato = 0;
+
+for (let atual = 0; atual < livros.length; atual++) {
+  if (livros[atual].preco < livros[maisBarato].preco) {
+    maisBarato = atual;
+  }
+}
+
+console.log(
+  `O livro mais barato custa R$ ${livros[maisBarato].preco},00 e o título é ${livros[maisBarato].titulo}`
+);
